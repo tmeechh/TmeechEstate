82dc1ce176a1f8cbc463eec167da4727f1ab7c92
Esan Taiwo <145122266+tmeechh@users.noreply.github.com>
2024-07-02 23:49:04 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import About from './pages/About.jsx';
+import Profile from './pages/Profile.jsx';
+
+
 const App = () => {
-  return <div className="bg-[tomato]">App</div>;
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/> }/>
+    </Routes>
+    </BrowserRouter>;
 };
 
 export default App;
