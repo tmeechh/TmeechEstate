10113c8b8a844ee208e05396645a25f59abdf496
Esan Taiwo <145122266+tmeechh@users.noreply.github.com>
2024-07-11 14:28:14 +0100

    complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,12 +7,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "tmeech-estate.firebaseapp.com",
-  projectId: "tmeech-estate",
-  storageBucket: "tmeech-estate.appspot.com",
-  messagingSenderId: "881897670490",
-  appId: "1:881897670490:web:e6cdde074cbc1ad99b5563"
+  authDomain: "meech-estatev2.firebaseapp.com",
+  projectId: "meech-estatev2",
+  storageBucket: "meech-estatev2.appspot.com",
+  messagingSenderId: "259189302347",
+  appId: "1:259189302347:web:63cc090a76914b5ff259ce"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export  const app = initializeApp(firebaseConfig);
